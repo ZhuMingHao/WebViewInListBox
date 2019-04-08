156101f3949fbaeac7b4bc20f800ace5bffa56f7
v-zhumin <v-zhumin@DESKTOP-KNMM69A>
2019-04-08 18:02:53 +0800

    Add project files.

---
+++ WebViewInListBox/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace WebViewInListBox
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public ObservableCollection<DBOption> Itmes { get; set; } = new ObservableCollection<DBOption>();
+      
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Itmes.Add(new DBOption { Option = @"<html><body><b>Hi there</b></body></html>", OID = 9527 });
+            Itmes.Add(new DBOption { Option = @"<p>2 x 16 menit</p>", OID = 9528 });
+        }
+
+       
+    }
+    public class DBOption
+    {
+        public string Option { get; set; }
+        public int OID { get; set; }
+    }
+    public class MyProperties
+    {
+        // "HtmlString" attached property for a WebView
+        public static readonly DependencyProperty HtmlStringProperty =
+           DependencyProperty.RegisterAttached("HtmlString", typeof(string), typeof(MyProperties), new PropertyMetadata("", OnHtmlStringChanged));
+
+        // Getter and Setter
+        public static string GetHtmlString(DependencyObject obj) { return (string)obj.GetValue(HtmlStringProperty); }
+        public static void SetHtmlString(DependencyObject obj, string value) { obj.SetValue(HtmlStringProperty, value); }
+
+        // Handler for property changes in the DataContext : set the WebView
+        private static void OnHtmlStringChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            WebView wv = d as WebView;
+            if (wv != null)
+            {
+                wv.NavigateToString((string)e.NewValue);
+            }
+        }
+    }
+}
